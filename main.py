605ea3778aaaf79ed6d8d01e6f9beb4bb8e99aaf
thug-creator <mr.pup06@mail.ru>
2025-10-01 11:14:50 +0300

    1

--- main.py
+++ main.py
@@ -9,9 +9,14 @@ class Rectangle:
     def get_height(self):
         return self._a
     
-    def get_width(self):
+     def get_width(self):
         return self._b
 
+    def set_height(self, a):
+        self._a = a
+
+    def set_width(self, b):
+        self._b = b
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())

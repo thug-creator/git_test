c7a444516e0c99b5f2d5965d76440f48198f74a0
thug-creator <mr.pup06@mail.ru>
2025-10-01 12:04:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,8 @@ class Rectangle:
 
     def get_height(self):
         return self._a
-    
-<<<<<<< HEAD
-     def get_width(self):
-=======
+
     def get_width(self):
->>>>>>> new_class
         return self._b
 
     def set_height(self, a):
@@ -33,10 +29,8 @@ class Circle:
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
-<<<<<<< HEAD
 main_rect.set_height(7)
 main_rect.set_width(8)
-=======
+
 main_circle = Circle(2)
 print(main_circle.area())
->>>>>>> new_class

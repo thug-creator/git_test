e42ed13faca7d8ca73cdd1b9b506cf06fb8c21d3
thug-creator <mr.pup06@mail.ru>
2025-10-01 11:04:00 +0300

    Создали новый Класс

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ class Rectangle:
         self._a = a
         self._b = b
 
-        def area(self):
-            return self._a * self._b
+    def area(self):
+        return self._a * self._b
 
-        def get_height(self):
-            return self._a
+    def get_height(self):
+        return self._a
     
-        def get_width(self):
-            return self._b
+    def get_width(self):
+        return self._b
 
+
+class Circle:
+    def __init__(self, r):
+        self._r = r
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
+
+main_circle = Circle(2)
